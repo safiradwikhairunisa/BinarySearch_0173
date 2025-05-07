aee6fba45eef5ff3166d9dcaeed5f08000b54f42
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-07 14:53:42 +0700

    add input

--- binary.cpp
+++ binary.cpp
@@ -4,3 +4,20 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+}
