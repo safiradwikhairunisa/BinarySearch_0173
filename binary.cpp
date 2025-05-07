87d1251917b36796562c233abd4f8061fdd1dae5
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-07 15:19:10 +0700

    step 3-5

--- binary.cpp
+++ binary.cpp
@@ -83,8 +83,18 @@ void binnarySearch()
         {
             int mid = (low + high) /2;
             if (element[mid] == x)
-            
+            {
+                cout << "\n[~] Element " << x << "ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
         }
     }
-    
 }
