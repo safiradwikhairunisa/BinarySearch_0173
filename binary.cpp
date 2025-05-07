bfcf3a5de6f69ed3c0abe0028345f21ab7c51497
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-07 14:58:33 +0700

    input element array

--- binary.cpp
+++ binary.cpp
@@ -20,4 +20,13 @@ void input()
             cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n==============================\n";
+    cout << "    Masukkan element array      \n";
+    cout << "================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i+1) << " = ";
+        cin >> element [i];
+    }
 }
