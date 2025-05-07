c081952b46053e09e5992610e85fad88b3779023
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-07 15:09:17 +0700

    void display

--- binary.cpp
+++ binary.cpp
@@ -48,3 +48,19 @@ void bubbleSortArray()
         pass++;
     }while (pass <= nPanjang - 1);
 }
+
+void display()
+{
+    cout << "\n==========================================\n";
+    cout << "   Element array setelah diurutkan (Asc)    \n";
+    cout << "\n==========================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -->";
+        }
+    }
+    cout << endl;
+}
